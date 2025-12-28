306d41b8bf89bee3f4ff69ab7843eb2281ffc9d9
alchemistReturns <abrarhasan2003@gmail.com>
2025-12-28 08:42:17 +0600

    Minor fixes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAi5gaFbgfrYngr08E89mHztAilSGgaOAY",
-    authDomain: "ieeensu-7bdab.firebaseapp.com",
-    projectId: "ieeensu-7bdab",
-    storageBucket: "ieeensu-7bdab.firebasestorage.app",
-    messagingSenderId: "540298386646",
-    appId: "1:540298386646:web:e1bed033a06d9c8446fd77",
-    measurementId: "G-00JVRJQ2DF"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
