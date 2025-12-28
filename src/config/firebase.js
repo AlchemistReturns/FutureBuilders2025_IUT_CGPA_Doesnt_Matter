48e486565a0796b0fc0d8148339c155b7cccc429
alchemistReturns <abrarhasan2003@gmail.com>
2025-12-28 08:35:50 +0600

    Initial Commit

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAi5gaFbgfrYngr08E89mHztAilSGgaOAY",
+    authDomain: "ieeensu-7bdab.firebaseapp.com",
+    projectId: "ieeensu-7bdab",
+    storageBucket: "ieeensu-7bdab.firebasestorage.app",
+    messagingSenderId: "540298386646",
+    appId: "1:540298386646:web:e1bed033a06d9c8446fd77",
+    measurementId: "G-00JVRJQ2DF"
+};
+
+const app = initializeApp(firebaseConfig);
